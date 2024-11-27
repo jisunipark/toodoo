d45310792f7f9a95b8575a7b3a91a244345025d8
Jisun Park <jisunpark.t@gmail.com>
2024-11-27 12:53:14 +0900

    feat: 아이템 hover 시 편집/삭제 아이콘 보여주기

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const $items = document.querySelectorAll('.item');
 [...$items].forEach(($item) => {
   const $input = $item.firstElementChild;
 
-  console.log($item.lastElementChild);
-
   // input에서 체크 표시가 된 아이템에는 done 클래스를 추가
   $input.addEventListener('click', () => {
     $item.classList.toggle('done');
+  });
+
+  // 아이템에 마우스 호버 시 편집/삭제 아이콘 나타나도록
+  $item.addEventListener('mouseover', () => {
+    $item.classList.add('hover');
+  });
+
+  $item.addEventListener('mouseleave', () => {
+    $item.classList.remove('hover');
   });
 });
