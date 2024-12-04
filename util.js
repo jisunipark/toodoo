911f92327126b59af1c90df96996b793a3dcf4dc
Jisun Park <jisunpark.t@gmail.com>
2024-12-04 17:46:28 +0900

    chore: 날짜 형식 yy. m. d로 수정

--- util.js
+++ util.js
@@ -1,8 +1,8 @@
 // 매개변수로 받은 Date 객체를 'yy/mm/dd' 형식의 문자열로 변환하여 반환하는 함수
 export const getFormattedDate = (date) => {
-  const yy = date.getFullYear();
+  const yy = String(date.getFullYear()).slice(2, 4);
   const mm = date.getMonth() + 1;
   const dd = date.getDate();
 
-  return `${yy}/${mm}/${dd}`;
+  return `${yy}. ${mm}. ${dd}.`;
 };
